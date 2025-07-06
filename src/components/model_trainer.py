e6a40ed73247a9893a546ed2cd02cc1fa2dbb0f2
Vivek-kumar-v <patelvivek6370@gmail.com>
2025-07-06 13:44:52 +0530

    Project Completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -29,7 +29,7 @@ from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","modle.pkl")
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
     
 class ModelTrainer:
     def __init__(self):
